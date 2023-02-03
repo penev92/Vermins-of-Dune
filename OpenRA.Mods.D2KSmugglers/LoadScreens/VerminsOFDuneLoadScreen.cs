ed12b1299400fd732852dda31de82715847a6300
oldnaari <daniil.hayrapetyan@gmail.com>
2023-02-04 01:36:17 +0400

    Change Loading Screens

---
+++ OpenRA.Mods.D2KSmugglers/LoadScreens/VerminsOFDuneLoadScreen.cs
@@ -0,0 +1,63 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+
+namespace OpenRA.Mods.Common.LoadScreens
+{
+	public sealed class VerminsOFDuneLoadScreen : SheetLoadScreen
+	{
+		float2 logoPos;
+		Sprite logo;
+
+		Sheet lastSheet;
+		int lastDensity;
+		Size lastResolution;
+
+		string[] messages = { "Loading..." };
+
+		public override void Init(ModData modData, Dictionary<string, string> info)
+		{
+			base.Init(modData, info);
+
+			if (info.ContainsKey("Text"))
+				messages = info["Text"].Split(',');
+		}
+
+		public override void DisplayInner(Renderer r, Sheet s, int density)
+		{
+			if (s != lastSheet || density != lastDensity)
+			{
+				lastSheet = s;
+				lastDensity = density;
+				logo = CreateSprite(s, density, new Rectangle(0, 0, 512, 512));
+			}
+
+			if (r.Resolution != lastResolution)
+			{
+				lastResolution = r.Resolution;
+				logoPos = new float2(lastResolution.Width / 2 - 256, lastResolution.Height / 2 - 256);
+			}
+
+			if (logo != null)
+				r.RgbaSpriteRenderer.DrawSprite(logo, logoPos);
+
+			if (r.Fonts != null)
+			{
+				var text = messages.Random(Game.CosmeticRandom);
+				var textSize = r.Fonts["Bold"].Measure(text);
+				r.Fonts["Bold"].DrawText(text, new float2(r.Resolution.Width - textSize.X - 20, r.Resolution.Height - textSize.Y - 20), Color.White);
+			}
+		}
+	}
+}
